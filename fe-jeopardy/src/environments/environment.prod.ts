8d22254bc0d10e8a432571dd61d25d287f56a053
David Cyprian <102708212+dcyprianrw@users.noreply.github.com>
2025-11-22 08:52:41 -0500

    hotfix env vars

--- fe-jeopardy/src/environments/environment.prod.ts
+++ fe-jeopardy/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
 	websocketProtocol: 'wss',
 	httpProtocol: 'https',
 	gameLink: 'trextrivia.com',
-	supabaseUrl: '',
-	supabaseKey: '',
+	supabaseUrl: 'https://xdlhyjzjygansfeoguvs.supabase.co',
+	supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbGh5anpqeWdhbnNmZW9ndXZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY5OTUwMjksImV4cCI6MjAyMjU3MTAyOX0.ystMHS-Tic8W3rHqXTwW1F90WvxfVHpLJ5bkimn81PM',
 	redirectUrl: 'https://trextrivia.com/join',
 	passwordRedirectUrl: 'https://trextrivia.com/password-reset',
 	showBirthdayMessage: true
